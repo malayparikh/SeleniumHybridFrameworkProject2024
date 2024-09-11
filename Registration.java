d14f038f45d0da8d65f0744c1e4768d89248ec49
Malay Parikh <63942863+malayparikh@users.noreply.github.com>
2024-09-11 18:27:04 +0530

    Update Registration.java

--- Registration.java
+++ Registration.java
@@ -5,14 +5,42 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.seleniumproject.qa.tests.Utilities;
+
 public class Registration {
+	
+	WebDriver driver;
+	
+	@AfterMethod
+	public void teadDown() {
 
-	@Test
-	public void VerifyUserRegistration() throws InterruptedException {
+		System.out.println("After Method- Close Browser");
 
-		WebDriver driver = new ChromeDriver();
+		driver.quit();
+
+	}
+
+	@BeforeMethod
+	public void setUp() {
+
+		String browserName = "edge";
+
+		if (browserName.equals("chrome")) {
+
+			driver = new ChromeDriver();
+		} else if (browserName.equals("firefox")) {
+
+			driver = new FirefoxDriver();
+		} else if (browserName.equals("edge")) {
+
+			driver = new EdgeDriver();
+		}
 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -20,17 +48,27 @@ public class Registration {
 		driver.findElement(By.linkText("My Account")).click();
 
 		driver.findElement(By.linkText("Register")).click();
+
+	}
+
+	@Test
+	public void VerifyUserRegistrationWithAlreadyRegisteredUser() throws InterruptedException {
+
+	
 		driver.findElement(By.id("input-firstname")).sendKeys("demouser1234");
 		driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("demolname");
-		driver.findElement(By.id("input-email")).sendKeys("demouser1234@demoemail.com");
+		driver.findElement(By.id("input-email")).sendKeys(Utilities.generateEmailTimestamp());
 		driver.findElement(By.id("input-telephone")).sendKeys("8979879879");
 		driver.findElement(By.id("input-password")).sendKeys("123456");
 		driver.findElement(By.id("input-confirm")).sendKeys("123456");
 		driver.findElement(By.xpath("//input[@name='agree']")).click();
 		driver.findElement(By.xpath("//input[@value='Continue']")).click();
-
+		
 		Thread.sleep(4000);
 
+	
 	}
+	
+
 
 }
