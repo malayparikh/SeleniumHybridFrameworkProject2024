62ca95be435822d0eb691f015c88492d97f56efe
Malay Parikh <63942863+malayparikh@users.noreply.github.com>
2024-10-04 18:25:24 +0530

    Update Utilities.java

--- Utilities.java
+++ Utilities.java
@@ -4,6 +4,9 @@ import java.util.Date;
 
 public class Utilities {
 	
+	public static final int IMPLICIT_WAIT_TIME=10;
+	public static final int PAGE_LOAD_TIME=5;
+	
 	public static String generateEmailTimestamp() {
 
 		Date date = new Date();
